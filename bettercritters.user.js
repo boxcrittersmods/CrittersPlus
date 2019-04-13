40b2c70a65762fe0fa1cb5b847cbe6beca59381f
slaggo <35737659+slaggo@users.noreply.github.com>
2019-04-13 19:46:04 -0400

    Update bettercritters.user.js

--- bettercritters.user.js
+++ bettercritters.user.js
@@ -14,7 +14,6 @@ var jokes = [
     {"j":"What do you call a hamster with no legs?","p":"A furball!"},
     {"j":"What do you call a hamster that can't run in a wheel?","p":"Hamateur"},
     {"j":"Why was the hamster upset with his job?","p":"It didn't pay enough celery"},
-    {"j":"What do you call a hamster that can't run in a wheel?","p":"Hamateur"},
     {"j":"What do you call a hamster with three legs?","p":"Hamputee"}
 ]
 
