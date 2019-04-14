16d1fee890856649ef51116774bc6856ec4fafcb
slaggo <35737659+slaggo@users.noreply.github.com>
2019-04-13 22:04:11 -0400

    Update bettercritters.user.js

--- bettercritters.user.js
+++ bettercritters.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         BetterCritters
+// @name         Critters+
 // @namespace    http://tampermonkey.net/
 // @version      1.1.2
 // @description  try to take over the world!
