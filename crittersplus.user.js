4bb2aec4cdf627d7983017b7f92f4310ee1145aa
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-07-19 10:38:31 -0400

    manual code cleanup and spelling fixes

--- crittersplus.user.js
+++ crittersplus.user.js
@@ -33,29 +33,14 @@ var chatBox = document.getElementsByClassName(
 )[1];
 
 var jokes = [
-	{
-		j: "What do you call a hamster in a tophat?",
-		p: "Abrahamster Lincoln!",
-	},
+	{ j: "What do you call a hamster in a tophat?", p: "Abrahamster Lincoln!", },
 	{ j: "Where does a hamster go for vacation?", p: "Hamsterdam!" },
 	{ j: "What do you call a hamster with no legs?", p: "A furball!" },
-	{
-		j: "What do you call a hamster that can't run in a wheel?",
-		p: "Hamateur.",
-	},
-	{
-		j: "Why was the hamster upset with his job?",
-		p: "It didn't pay enough celery.",
-	},
+	{ j: "What do you call a hamster that can't run in a wheel?", p: "Hamateur.", },
+	{ j: "Why was the hamster upset with his job?", p: "It didn't pay enough celery.", },
 	{ j: "What do you call a hamster with three legs?", p: "Hamputee." },
-	{
-		j: "What happens when two snails get into a fight?",
-		p: "They slug it out!",
-	},
-	{
-		j: "Why is the snail the strongest animal?",
-		p: "Because he carries a house on his back!",
-	},
+	{ j: "What happens when two snails get into a fight?", p: "They slug it out!", },
+	{ j: "Why is the snail the strongest animal?", p: "Because he carries a house on his back!", },
 	{ j: "How do snails make important calls?", p: "On shell phones." },
 	{ j: "What kind of car does a raccoon drive?", p: "A furrari." },
 ];
@@ -79,20 +64,19 @@ function camelize(str) {
 
 
 if (!BCMacro) {
-	{
-		let dialogueHTML = `<div id="CP_modal" class="modal fade" tabindex="-1" role="dialog">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header"><button type="button" class="close" data-dismiss="CP_model" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				  </button></div>
-					<div class="modal-body"></div>
-					<div class="modal-footer"></div>
+	document.body.insertAdjacentHTML("afterbegin", `<div id="CP_modal" class="modal fade" tabindex="-1" role="dialog">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="CP_model" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
 				</div>
+				<div class="modal-body"></div>
+				<div class="modal-footer"></div>
 			</div>
-		</div>`;
-		document.body.insertAdjacentHTML("afterbegin", dialogueHTML);
-	}
+		</div>
+	</div>`);
 
 	function createDialogue(header, body, footer) {
 		$("#CP_modal").modal();
@@ -102,9 +86,9 @@ if (!BCMacro) {
 		if (footer) $("#CP_modal .modal-footer").html(footer);
 		return $("#CP_model");
 	}
-	createDialogue("Macro Info", `
-	The Macros API has grown apart from Criters plus to become its own API only mod.
-	Please click the link below to install. <strong>Make sure to uninstall Critters+ and reinstall after you have installed the macro API as this will cause problems when installed out of order.</strong>`,
+	createDialogue("Macro Info",
+		`The Macros API has grown apart from Critters+ to become its own API only mod.
+Please click the link below to install. <strong>Make sure to uninstall Critters+ and reinstall after you have installed the macro API as this will cause problems when installed out of order.</strong>`,
 		'<a class="btn btn-primary" href="https://boxcrittersmods.ga/mods/bcmacro-api/">Install Macro API</a>');
 }
 
@@ -123,8 +107,7 @@ window.addEventListener("load", async function () {
 	function sendClap() {
 		var message = document.getElementById("inputMessage").value;
 		document.getElementById("inputMessage").value = "";
-		message = message.split(" ").join(" 👏 ");
-		message = "👏" + message + "👏";
+		message = `👏 ${message.replace(" ", " 👏 ")} 👏`;
 		console.log("[CP]", message);
 		world.sendMessage(message);
 	}
@@ -172,8 +155,11 @@ window.addEventListener("load", async function () {
 
 	//-------------------------------------------------------------------------------------------------------------------------
 
-	var darkmodeHTML = `<div id="dmDiv" class="row justify-content-center"><span><input class="form-check-input" type="checkbox" value="" id="darkmode"><label class="form-check-label" for="darkmode" style="color:#696f75;">Dark Mode</label></span></div>`;
-	chatBox.insertAdjacentHTML("afterend", darkmodeHTML);
+	chatBox.insertAdjacentHTML("afterend",
+		`<div id="dmDiv" class="row justify-content-center"><span>
+			<input class="form-check-input" type="checkbox" value="" id="darkmode">
+			<label class="form-check-label" for="darkmode" style="color:#696f75;">Dark Mode</label>
+		</span></div>`); // darkmode html
 
 	if (localStorage.getItem("theme") == "dark") {
 		document.body.style = "background-color:rgb(16, 21, 31);transition:0.5s;";
@@ -183,18 +169,16 @@ window.addEventListener("load", async function () {
 	function darkmodeToggle() {
 		if (darkmodeBox.checked == true) {
 			localStorage.setItem("theme", "dark");
-			document.body.style =
-				"background-color:rgb(16, 21, 31);transition:0.5s;";
+			document.body.style = "background-color:rgb(16, 21, 31);transition:0.5s;";
 		} else {
 			localStorage.setItem("theme", "light");
 			document.body.style = "background-color:#f7f7f7;transition:0.5s;";
 		}
 	}
 
-	var redeemallitemsBtn = document.querySelector("#redeemallitemsbtn");
-	if (redeemallitemsBtn) {
-		redeemallitemsBtn.addEventListener("click", redeemallitems, false);
-	}
+	let raib = document.getElementById("redeemallitemsbtn");
+	if (raib)
+		raib.addEventListener("click", raib, false);
 },
 	false
 );
