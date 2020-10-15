360491a4f131a30e928e9fcd52acb634216b9aea
Tumble <tumblegamer@gmail.com>
2020-10-15 11:00:08 +0100

    using simpler apis

--- crittersplus.user.js
+++ crittersplus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Critters+
 // @namespace    http://discord.gg/G3PTYPy
-// @version      2.3.24.57
+// @version      2.3.25.58
 // @description  Adds new features to BoxCritters to improve your experience!
 // @author       slaggo,TumbleGamer
 // @match        https://boxcritters.com/play/
@@ -13,19 +13,21 @@
 // @require      https://code.jquery.com/jquery-3.5.1.slim.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.4.0/umd/popper.min.js
 // @require      https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js
+// @require      https://github.com/tumble1999/mod-utils/raw/master/mod-utils.js
+// @require      https://github.com/tumble1999/native-modals/raw/master/native-modal.js
 // @icon         https://raw.githubusercontent.com/boxcritters/CrittersPlus/master/icon.png
 // @updateURL    https://github.com/boxcritters/CrittersPlus/raw/master/crittersplus.user.js
 // @run-at       document-end
 // @grant        unsafeWindow
 // ==/UserScript==
 
-console.info("-----------------------------------");
-console.info("[CRITTERS+]");
-console.info("A mod created by Slaggo, current development under TumbleGamer");
-console.info("-----------------------------------");
-
 window = unsafeWindow || window;
-var CrittersPlus = {};
+var mod = BCModUtils.InitialiseMod({
+	name: "Critters+",
+	abriv: "CP",
+	author: "Slaggo and TumbleGamer"
+})
+var CrittersPlus = {mod:mod};
 var BCMacros = window.BCMacros;
 window.CrittersPlus = CrittersPlus;
 var chatBox = document.getElementsByClassName(
@@ -63,7 +65,7 @@ var jokes = [
 
 // Code for delay function
 var delay = (function () {
-	var timer = 0;
+	let timer = 0;
 	return function (callback, ms) {
 		clearTimeout(timer);
 		timer = setTimeout(callback, ms);
@@ -79,156 +81,128 @@ function camelize(str) {
 
 
 if (!BCMacros) {
-	{
-		let dialogueHTML = `<div id="CP_modal" class="modal fade" tabindex="-1" role="dialog">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header"><button type="button" class="close" data-dismiss="CP_model" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				  </button></div>
-					<div class="modal-body"></div>
-					<div class="modal-footer"></div>
-				</div>
-			</div>
-		</div>`;
-		document.body.insertAdjacentHTML("afterbegin", dialogueHTML);
-	}
-
-	function createDialogue(header, body, footer) {
-		$("#CP_modal").modal();
-		$("#CP_modal").modal("show");
-		if (header) $("#CP_modal .modal-header").html(header);
-		if (body) $("#CP_modal .modal-body").html(body);
-		if (footer) $("#CP_modal .modal-footer").html(footer);
-		return $("#CP_model");
-	}
-	createDialogue("Macro Info", /*`
-	/*The Macros API has grown apart from Criters plus to become its own API only mod.
-	Please click the link below to install. <strong>Make sure to uninstall Critters+ and reinstall after you have installed the macro API as this will cause problems when installed out of order.</strong>`,
-		'<a class="btn btn-primary" href="https://boxcrittersmods.ga/mods/bcmacro-api/">Install Macro API</a>'*/`
-		There was a error loading BCMacro mod`,'<a class="btn btn-primary" href="https://boxcrittersmods.ga/mods/bcmacro-api/">Install Macro API</a>')
+	var modal = new BSModal();
+	modal.setContent({
+		header: `Macro Info`+BSModal.closeButton,
+		body: `The Macros API has grown apart from Criters plus to become its own API only mod.
+		Please click the link below to install. <strong>Make sure to uninstall Critters+ and reinstall after you have installed the macro API as this will cause problems when installed out of order.</strong>`,
+		footer: `<a class="btn btn-primary" href="https://boxcrittersmods.ga/mods/bcmacro-api/">Install Macro API</a>`
+	});
+	modal.show();
 }
 
 // Runs on page load
 
-	function sendJoke() {
-		document.getElementById("message").value = "";
-		var joke = jokes[Math.floor(Math.random() * jokes.length)]; // Retrieve random joke from variable
-		BCMacros.sendMessage(joke.j); // Send the first part of the joke
-		delay(function () {
-			BCMacros.sendMessage(joke.p); // Send the punchline
-		}, 5000); // end delay
-	}
+function sendJoke() {
+	document.getElementById("message").value = "";
+	let joke = jokes[Math.floor(Math.random() * jokes.length)]; // Retrieve random joke from variable
+	BCMacros.sendMessage(joke.j); // Send the first part of the joke
+	delay(function () {
+		BCMacros.sendMessage(joke.p); // Send the punchline
+	}, 5000); // end delay
+}
 
-	function sendClap() {
-		var message = document.getElementById("message").value;
-		document.getElementById("message").value = "";
-		message = message.split(" ").join(" 👏 ");
-		message = "👏" + message + "👏";
-		console.log("[CP]", message);
-		BCMacro.sendMessage(message);
-	}
+function sendClap() {
+	let message = document.getElementById("message").value;
+	document.getElementById("message").value = "";
+	message = message.split(" ").join(" 👏 ");
+	message = "👏" + message + "👏";
+	mod.log(message);
+	BCMacro.sendMessage(message);
+}
 
-	CrittersPlus.sendJoke = sendJoke;
-	CrittersPlus.sendClap = sendClap;
-	{
-		console.log("[CP] Setting up macros...");
-		var cpMacros = BCMacros.CreateMacroPack({
-			name: "Critters Plus"
-		})
-		cpMacros.createMacro({
-			name: "Joke",
-			action: CrittersPlus.sendJoke,
-			button: {
-				color:"warning"
-				//color: "#28a245"
-			}
-		})
-		/*cpMacros.createMacro({
-			name: "Clap",
-			action: CrittersPlus.sendClap,
-			button: {
-				color: "#ffc107"
-			}
-		})*/
-		cpMacros.createMacro({
-			name: "Chat Balloons",
-			action: _ => {
-				world.stage.room.balloons.visible ^= true;
-			},
-			button: {}
-		})
-		cpMacros.createMacro({
-			name: "NameTags",
-			action: _ => {
-				world.stage.room.nicknames.visible ^= true;
-			},
-			button: {}
-		})
+CrittersPlus.sendJoke = sendJoke;
+CrittersPlus.sendClap = sendClap;
+if(BCMacros) {
+	mod.log("Setting up macros...");
+	var cpMacros = BCMacros.CreateMacroPack({
+		name: "Critters Plus"
+	})
+	cpMacros.createMacro({
+		name: "Joke",
+		action: CrittersPlus.sendJoke,
+		button: {
+			color: "warning"
+		}
+	})
+	cpMacros.createMacro({
+		name: "Chat Balloons",
+		action: _ => {
+			world.stage.room.balloons.visible ^= true;
+		},
+		button: {}
+	})
+	cpMacros.createMacro({
+		name: "NameTags",
+		action: _ => {
+			world.stage.room.nicknames.visible ^= true;
+		},
+		button: {}
+	})
 
-		cpMacros.createMacro({
-			name: "freeitem",
-			action: _ => {
-				BCMacros.sendMessage("/freeitem");
-			},
-			button: {}
-		})
+	cpMacros.createMacro({
+		name: "freeitem",
+		action: _ => {
+			BCMacros.sendMessage("/freeitem");
+		},
+		button: {}
+	})
 
-		cpMacros.createMacro({
-			name: "pop",
-			action: _ => {
-				BCMacros.sendMessage("/pop");
-			},
-			button: {}
-		})
+	cpMacros.createMacro({
+		name: "pop",
+		action: _ => {
+			BCMacros.sendMessage("/pop");
+		},
+		button: {}
+	})
 
-		cpMacros.createMacro({
-			name: "beep",
-			action: _ => {
-				BCMacros.sendMessage("/beep");
-			},
-			button: {}
-		})
+	cpMacros.createMacro({
+		name: "beep",
+		action: _ => {
+			BCMacros.sendMessage("/beep");
+		},
+		button: {}
+	})
 
-		cpMacros.createMacro({
-			name: "darkmode",
-			action: _ => {
-				BCMacros.sendMessage("/darkmode");
-			},
-			button: {}
-		})
+	cpMacros.createMacro({
+		name: "darkmode",
+		action: _ => {
+			BCMacros.sendMessage("/darkmode");
+		},
+		button: {}
+	})
 
-		cpMacros.createMacro({
-			name: "game",
-			action: _ => {
-				BCMacros.sendMessage("/game");
-			},
-			button: {}
-		})
-	}
+	cpMacros.createMacro({
+		name: "game",
+		action: _ => {
+			BCMacros.sendMessage("/game");
+		},
+		button: {}
+	})
+}
 
 	//-------------------------------------------------------------------------------------------------------------------------
 
-	/*var darkmodeHTML = `<div id="dmDiv" class="row justify-content-center"><span><input class="form-check-input" type="checkbox" value="" id="darkmode"><label class="form-check-label" for="darkmode" style="color:#696f75;">Dark Mode</label></span></div>`;
-	chatBox.insertAdjacentHTML("afterend", darkmodeHTML);
+/*var darkmodeHTML = `<div id="dmDiv" class="row justify-content-center"><span><input class="form-check-input" type="checkbox" value="" id="darkmode"><label class="form-check-label" for="darkmode" style="color:#696f75;">Dark Mode</label></span></div>`;
+chatBox.insertAdjacentHTML("afterend", darkmodeHTML);
 
-	if (localStorage.getItem("theme") == "dark") {
-		document.body.style = "background-color:rgb(16, 21, 31);transition:0.5s;";
-		document.getElementById("darkmode").checked = true;
+if (localStorage.getItem("theme") == "dark") {
+	document.body.style = "background-color:rgb(16, 21, 31);transition:0.5s;";
+	document.getElementById("darkmode").checked = true;
+}
+
+function darkmodeToggle() {
+	if (darkmodeBox.checked == true) {
+		localStorage.setItem("theme", "dark");
+		document.body.style =
+			"background-color:rgb(16, 21, 31);transition:0.5s;";
+	} else {
+		localStorage.setItem("theme", "light");
+		document.body.style = "background-color:#f7f7f7;transition:0.5s;";
 	}
+}
 
-	function darkmodeToggle() {
-		if (darkmodeBox.checked == true) {
-			localStorage.setItem("theme", "dark");
-			document.body.style =
-				"background-color:rgb(16, 21, 31);transition:0.5s;";
-		} else {
-			localStorage.setItem("theme", "light");
-			document.body.style = "background-color:#f7f7f7;transition:0.5s;";
-		}
-	}
-
-	var redeemallitemsBtn = document.querySelector("#redeemallitemsbtn");
-	if (redeemallitemsBtn) {
-		redeemallitemsBtn.addEventListener("click", redeemallitems, false);
-	}*/
+var redeemallitemsBtn = document.querySelector("#redeemallitemsbtn");
+if (redeemallitemsBtn) {
+	redeemallitemsBtn.addEventListener("click", redeemallitems, false);
+}*/
