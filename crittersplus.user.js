c699bf86691b77ad914c02d0367432f2154c37d0
Tumble <tumblegamer@gmail.com>
2020-10-15 11:59:51 +0100

    version

--- crittersplus.user.js
+++ crittersplus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Critters+
 // @namespace    http://discord.gg/G3PTYPy
-// @version      2.3.25.58
+// @version      2.3.26.59
 // @description  Adds new features to BoxCritters to improve your experience!
 // @author       slaggo,TumbleGamer
 // @match        https://boxcritters.com/play/
